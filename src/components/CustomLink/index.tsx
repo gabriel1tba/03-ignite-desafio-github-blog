2c5861811810eaa7e5ac54f6ca7e4f2f94495bc3
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2022-12-19 10:27:46 -0300

    feat: aplicando polimorfismo no componente link

--- src/components/CustomLink/index.tsx
+++ src/components/CustomLink/index.tsx
@@ -1,23 +1,30 @@
+import {
+  forwardRef,
+  ForwardRefRenderFunction,
+  AnchorHTMLAttributes,
+} from 'react';
 import { IconType } from 'react-icons';
 import { LinkProps } from 'react-router-dom';
 
 import * as S from './styles';
 
-interface CustomLinkProps extends LinkProps {
+type CustomLinkTypes = AnchorHTMLAttributes<HTMLAnchorElement> | LinkProps;
+
+type CustomLinkProps = {
   rightIcon?: IconType;
   leftIcon?: IconType;
-}
+  as?: React.ElementType;
+} & CustomLinkTypes;
 
-const CustomLink = ({
-  children,
-  rightIcon: RightIcon,
-  leftIcon: LeftIcon,
-  ...props
-}: CustomLinkProps) => (
-  <S.Wrapper {...props}>
+const CustomLink: ForwardRefRenderFunction<CustomLinkTypes, CustomLinkProps> = (
+  { children, rightIcon: RightIcon, leftIcon: LeftIcon, ...props },
+  ref
+) => (
+  <S.Wrapper ref={ref} {...props}>
     {LeftIcon && <LeftIcon />}
     <span>{children}</span>
     {RightIcon && <RightIcon />}
   </S.Wrapper>
 );
-export default CustomLink;
+
+export default forwardRef(CustomLink);
